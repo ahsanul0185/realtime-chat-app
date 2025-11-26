941ba847c3933080c1e9a63b199ba73571dc2195
Ahsanul Haque <ahsanul0185@gmail.com>
2025-11-26 12:42:29 +0600

    update profile endpoint added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import cloudinary from "../lib/cloudinary.js";
 import { ENV } from "../lib/env.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -70,9 +70,12 @@ export const signup = async (req, res) => {
   }
 };
 
-
 export const login = async (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ message: "Email and password are required" });
+  }
 
   try {
     const user = await User.findOne({ email });
@@ -105,3 +108,26 @@ export const logout = (_, res) => {
   res.cookie("jwt", "", { maxAge: 0 });
   res.status(200).json({ message: "Logged out successfully" });
 };
+
+export const updateProfile = async (req, res) => {
+  try {
+
+    const {profilePic} = req.body;
+
+    if (!profilePic) {
+      return res.status(400).json({message : "Profile pic is required"});
+    }
+
+    const userId = req.user._id;
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic, { folder: "chat_app" });
+
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic : uploadResponse.secure_url}, {new : true});
+
+    res.status(200).json(updatedUser)
+
+  } catch (error) {
+    console.log("Error in update profile controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from 'cookie-parser'
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
@@ -11,6 +12,7 @@ const PORT = ENV.PORT || 3000;
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
