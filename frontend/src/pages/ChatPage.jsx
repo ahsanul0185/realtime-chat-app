f48b3c48532dacd690ca1a59210c0f32cfe6c819
Ahsanul Haque <ahsanul0185@gmail.com>
2025-11-30 00:33:47 +0600

    made the application mobile responsive

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -13,7 +13,9 @@ const ChatPage = () => {
   const {activeTab, selectedUser} = useChatStore();
 
   return (
-    <div className="relative w-full max-w-6xl flex h-[800px] rounded-2xl border border-gray-200/20 bg-linear-to-br from-slate-900 via-neutral-950 to-black">
+<>
+
+    <div className="hidden relative w-full max-w-6xl md:flex h-[800px] rounded-2xl border border-gray-200/20 bg-linear-to-br from-slate-900 via-neutral-950 to-black">
       {/* SIDEBAR */}
       <div className="w-80 border-r h-full border-r-gray-200/20 flex flex-col">
         <ProfileHeader/>
@@ -28,8 +30,25 @@ const ChatPage = () => {
       <div className="flex-1 flex flex-col">
         {selectedUser ? <ChatContainer /> : <NoConversation/>}
       </div>
-
     </div>
+
+
+        <div className="md:hidden relative w-full flex h-screen bg-linear-to-br from-slate-900 via-neutral-950 to-black">
+
+      {/* RIGHT SIDE */}
+      <div className="flex-1 flex flex-col">
+        {selectedUser ? <ChatContainer /> :       <div className="w-full h-full flex flex-col">
+        <ProfileHeader/>
+        <ActiveTabSwitch />
+
+        <div className="flex-1 overflow-y-auto p-1.5 space-y-2">
+          {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+        </div>
+      </div>}
+      </div>
+    </div>
+
+</>
   )
 }
 
